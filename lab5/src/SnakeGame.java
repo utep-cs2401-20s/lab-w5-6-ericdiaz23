7b71e81688e3743da05cadc0b87d0e73f549cf98
ediaz23 <Soxners$23>
2020-02-17 14:07:29 -0700

    Init.

---
+++ lab5/src/SnakeGame.java
@@ -0,0 +1,13 @@
+public class SnakeGame {
+
+    // Elements
+    private boolean[][]  game;
+    private int[] headPosition;
+    private static int exhaustiveChecks;
+    private static int recursiveChecks;
+
+    // Constructors
+
+
+    // Methods
+}
